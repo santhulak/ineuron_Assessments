5b85d1db0cbde34e8b7a818a5247ac3986de2e6a
Santhana Lakshmi <66127023+santhulak@users.noreply.github.com>
2023-07-02 12:09:10 +0530

    Add files via upload

---
+++ PPT/Assignments/mock test/Question_20.py
@@ -0,0 +1,28 @@
+'''Write a function called ‘perform_hypothesis_test’ that takes two lists of numbers as input, representing two samples. The function should perform a two-sample t-test and return the p-value. Use the ‘scipy.stats’ module in Python to calculate the t-test and p-value.
+
+Example:
+
+from scipy import stats
+
+def perform_hypothesis_test(sample1, sample2):
+    t_statistic, p_value = stats.ttest_ind(sample1, sample2)
+    return p_value
+
+sample1 = [5, 10, 15, 20, 25]
+sample2 = [10, 20, 30, 40, 50]
+p_value = perform_hypothesis_test(sample1, sample2)
+print("P-value:", p_value)
+
+Output:
+P-value: 0.1064706396450037
+8 points
+'''
+from scipy import stats
+
+def perform_hypothesis_test(sample1, sample2):
+    t_statistic, p_value = stats.ttest_ind(sample1, sample2)
+    return p_value
+sample1 = [5, 10, 15, 20, 25]
+sample2 = [10, 20, 30, 40, 50]
+p_value = perform_hypothesis_test(sample1, sample2)
+print("P-value:", p_value)
